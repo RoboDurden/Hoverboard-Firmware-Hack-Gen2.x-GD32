93a79bebcc6f6e8197c6ad767c403d6f08787539
Robo Durden <roland@robosoft.de>
2025-08-20 10:23:38 +0200

    PilotUser.c for custom code :-)

--- HoverBoardGigaDevice/Src/PilotUser.c
+++ HoverBoardGigaDevice/Src/PilotUser.c
@@ -14,6 +14,7 @@ extern uint8_t bPilotTimeout;		// set to 1 to disable bldc (with soft brake)
 
 // read/write
 extern uint8_t  wState;				// 1=ledGreen, 2=ledOrange, 4=ledRed, 8=ledUp, 16=ledDown   , 32=Battery3Led, 64=Disable, 128=ShutOff
+extern uint8_t iDrivingMode;	//  0=pwm, 1=speed in revs/s*1024, 2=torque in NewtonMeter*1024, 3=iOdometer
 
 // read only
 extern int32_t steer;		// -1000 to +1000
@@ -25,6 +26,7 @@ extern float realSpeed; 				// global variable for real Speed
 extern int32_t revs32;					// revs/s *1024
 extern int32_t torque32;				// torque in Nm*1024 
 extern DataSlave oDataSlave;	//	currentDC, realSpeed and iOdom and wState of (possible) slave
+extern uint16_t buzzerTimer;	// fastest timer available, counts upwards with PWM_FREQ, limited to 12 kHz for BLDC_SINE
 
 // write only:
 extern uint8_t  wStateSlave;	// wSate to be sent to (possible) slave
