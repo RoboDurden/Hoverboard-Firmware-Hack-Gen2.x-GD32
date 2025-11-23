e2c2dadd7ece39fb76e1439c6bba2145edb1f3c1
WestlingPi <westling@latitude>
2025-11-23 12:12:26 +0100

    Updates to RemoteROS2
    Pilot added to RemoteROS2.
    Added TestROS2 to be able to perform simple tests with ESP32 instead
    of full stack ROS2 controller.
    Calibrated 2-2-3 board battery voltage scaling-factor with 10s battery.
    
    Tested with 2-2-3 hoverboard (STM32 based) and TestROS2 on ESP32.

--- HoverBoardGigaDevice/Inc/RemoteROS2.h
+++ HoverBoardGigaDevice/Inc/RemoteROS2.h
@@ -1,6 +1,8 @@
 #ifndef REMOTE_ROS2_H
 #define REMOTE_ROS2_H
 
+#include <stdint.h>
+
 #define STAND_STILL_THRESHOLD 10
 
 #define REMOTE_BAUD 19200
@@ -9,7 +11,7 @@
 
 #define LOST_CONNECTION_STOP_MILLIS 500		// set speed to 0 when 500 ms no command received
 
-//#undef PILOT_DEFAULT
-//void 	Pilot(int16_t* pPwmMaster, int16_t* pPwmSlave);
+#undef PILOT_DEFAULT
+void 	Pilot(int16_t* pPwmMaster, int16_t* pPwmSlave);
 
 #endif // REMOTE_ROS2_H
