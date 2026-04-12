47746d211ec36679e9db45f00b5adf62b5b75074
hoverboardhavoc <hoverboardhavoc.county449@passmail.net>
2026-04-12 11:59:50 +0200

    Increase PWM frequency to 24 kHz (above audible range)
    Moves the switching noise out of human hearing. ISR at 12 µs fits
    comfortably in the 41.7 µs PWM period (29.7 µs headroom). Timer
    period, mid/min/max values all derive from PWM_FREQ so no other
    changes needed.

--- HoverBoardGigaDevice/Inc/config.h
+++ HoverBoardGigaDevice/Inc/config.h
@@ -80,7 +80,7 @@
 
 #define DC_CUR_LIMIT     		15        // Motor DC current limit in amps
 #define DEAD_TIME        		60        // PWM deadtime (60 = 1�s, measured by oscilloscope)
-#define PWM_FREQ         		16000     // PWM frequency in Hz
+#define PWM_FREQ         		24000     // PWM frequency in Hz (24k = above audible range)
 
 // ################################################################################
 
